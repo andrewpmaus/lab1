677ed62cbdc271f194104654811a65604d82a6d0
Andrew Maus <andrewpmaus@gmail.com>
2018-01-24 11:34:22 -0500

    Wednesday Class

---
+++ lab1/rgb_controller/rgb_controller.ino
@@ -0,0 +1,24 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+ //paste look-up table here from References in ES432 folder
+
+#define LED_R 6
+#define LED_G 5
+#define LED_B 3
+
+int val;
+// the setup routine runs once when you press reset:
+void setup() {                
+  Serial.begin(9600); //Open Serial Port
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  val = analogRead(A0);
+  analogWrite(LED_R, val/4);   // turn the LED on (HIGH is the voltage level)               // wait for a second
+}
